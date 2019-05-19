37458dfbe51d4dba3f1658b7ca30f7c91b7ba2de
kiril <kiril.g88@gmail.com>
2019-05-19 21:45:08 +0300

    added time picker scripts

--- NEW/Bazinga2/app/src/main/java/com/ork/bazinga2/fragments/TimerFragment.java
+++ NEW/Bazinga2/app/src/main/java/com/ork/bazinga2/fragments/TimerFragment.java
@@ -1,21 +1,53 @@
 package com.ork.bazinga2.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ork.bazinga2.MainApp;
 import com.ork.bazinga2.R;
 
-public class TimerFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+public class TimerFragment extends Fragment implements TimerUpdate.TimeUpdateListener {
     View curview;
     private MainApp mainApp;
+
+    static public TextView Timer;
+    CountDownTimer counter;
+    int max = 60;//(int)TimeUnit.HOURS.toSeconds(3);
+
+    long timeLeft = TimeUnit.SECONDS.toMillis(10);
+    ProgressBar progressBar;
+    //sub-subject TextView
+    TextView sbText;
+    //main subject TextView
+    TextView mnText;
+    Intent currentTimer;
+    boolean isruning = false;
+    //TODO:
+    // get sub-subject list from data base
+    // save to local storage and memory(arraylist)
+
+    //subject array
+    final ArrayList<String> subjects = new ArrayList<>();
+
+
+    public void hide(View view){ curview.findViewById(R.id.stop).setVisibility(view.INVISIBLE);}
+    public void show(View view){ curview.findViewById(R.id.stop).setVisibility(view.VISIBLE); }
 
     public void setPageTitle(String title) {
 
@@ -25,9 +57,167 @@ public class TimerFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+
         curview =  inflater.inflate(R.layout.exam_list_fragment,container,false);
         mainApp = new MainApp();
         setPageTitle("Timer");
+
+        Timer = curview.findViewById(R.id.title);
+        progressBar = curview.findViewById(R.id.progressBar);
+        progressBar.setMax(max);
+        subjects.add("aaa");
+        subjects.add("bbb");
+        subjects.add("ccc");
+
+        //set main subject
+        mnText = (TextView)curview.findViewById(R.id.txtMainSubject);
+        mnText.setText("AAA");
+        //set sub-subject
+        sbText = (TextView)curview.findViewById(R.id.txtSubSubject);
+        sbText.setText(subjects.get(0));
+
+
+        Button btnStart = curview.findViewById(R.id.btnStartTimer);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Start(v);
+            }
+        });
+        Button btnStopt = curview.findViewById(R.id.btnStartTimer);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Stop(v);
+            }
+        });
+
+
+
         return  curview;
     }
+
+
+
+
+    public void Start(final View view){
+        if( isruning ) {
+            return;
+        }
+
+        Log.i("infosss","start");
+        isruning = true;
+
+        show(curview.findViewById(R.id.stop));
+        counter = new CountDownTimer(timeLeft,1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //Log.i("info","onTick");
+                int secs =(int)millisUntilFinished / 1000;
+                progressBar.setProgress(  max - secs );
+                timeLeft = millisUntilFinished;
+                Log.i("timeLeft",Long.toString(timeLeft));
+                Update(secs);
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(curview.getContext(),"done " + subjects.get(0),Toast.LENGTH_LONG).show();
+                isruning = false;
+                counter.cancel();
+                //TODO:
+                // remove finished sub-subject from array
+                // update local storage
+                // get next sub-subject from local storage
+                // set up timers
+                // start again
+                subjects.remove(0);
+                //check if subject list is empty
+                //if empty done with current subject
+                if(!subjects.isEmpty() && subjects!= null)
+                {
+                    sbText.setText(subjects.get(0));
+                    start();
+                }
+                else{
+                    sbText.setText("no more subjects");
+                    Toast.makeText(curview.getContext(),"all done!",Toast.LENGTH_LONG).show();
+                    //TODO:
+                    // update data base
+                }
+            }
+        }.start();
+    }
+
+    public void Stop(View view){
+        counter.cancel();
+        isruning = false;
+
+    }
+
+
+    public void Update(int secLeft){
+        int hours =  secLeft / 3600;
+        int secondsLeft = secLeft - hours * 3600;
+        int secounds = secLeft % 60;
+        int minutes = secondsLeft / 60;
+        String minStr = minutes < 10 ? "0" + Integer.toString(minutes) : Integer.toString(minutes);
+        String houresStr = hours <10 ? "0" + Integer.toString(hours) : Integer.toString(hours);
+        String secoundsStr = secounds < 10 ? "0" + Integer.toString(secounds) : Integer.toString(secounds);
+        String str =  houresStr+ ":" + minStr + ":" + secoundsStr;
+        Timer.setText(str);
+    }
+
+    public void Add(int newTime){
+        //timeLeft += 3600000;
+        timeLeft+=newTime;
+        max = (int)(timeLeft / 1000);
+        counter.cancel();
+        isruning = false;
+        Start(null);
+        progressBar.setMax(max);
+        // progressBar.setProgress(max - TimeUnit.MILLISECONDS.toSeconds(timeLeft));
+    }
+
+    public void Done()
+    {
+        //TODO:
+        // update main progress bar(total learning progress)
+    }
+
+    private void _StorageUpdate(String key, String ... params)
+    {
+        //TODO:
+        // update the data in the local storage
+    }
+
+    private void _StorageInsart(String key,String value)
+    {
+        //TODO:
+        // insert data as json to local storage
+    }
+
+    private void _StorageGet(String key)
+    {
+        //TODO:
+        // pull json from data storage
+    }
+
+    public void _OpenTimeEditor(View view) {
+        Stop(null);
+        TimerUpdate updater = new TimerUpdate();
+        updater.show(mainApp.getSupportFragmentManager(),"update time");
+    }
+
+    @Override
+    public void ApplyNewTime(int newTime) {
+        Add(newTime);
+        Start(null);
+    }
+
+
+    //TODO:
+    // functions: -retrive data from local storage
+    //            -update locat storage
+    //            -get data from database
 }
---
+++ NEW/Bazinga2/app/src/main/java/com/ork/bazinga2/fragments/TimerUpdate.java
@@ -0,0 +1,106 @@
+package com.ork.bazinga2.fragments;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ork.bazinga2.R;
+
+public class TimerUpdate extends AppCompatDialogFragment {
+    //update listener
+    private TimeUpdateListener _listener;
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.timer_update,null);
+        //time fields
+        final TextView hours = (TextView)view.findViewById(R.id.timeHours);
+        final TextView minuts = (TextView)view.findViewById(R.id.timeMinuts);
+        //time filter for minutes between 0-59
+        minuts.setFilters(new InputFilter[]{ new InputFilterMinMax(0, 59)});
+
+        builder.setView(view)
+                .setTitle("edit clock")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //check if input is empty
+                String H = hours.getText().toString().isEmpty() ||  hours.getText().toString() == null? "0" : hours.getText().toString() ;
+                String M = minuts.getText().toString().isEmpty() ||  minuts.getText().toString() == null? "0" : minuts.getText().toString() ;
+
+                //convert to micro seconds
+                int h =  1000 * 60 * 60 * Integer.parseInt(H);
+                int m = 1000 * 60 * Integer.parseInt(M);
+                //update timer
+                _listener.ApplyNewTime(h+m);
+
+            }
+        });
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            _listener = (TimeUpdateListener)context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()+"must implement TimeUpdateListener");
+        }
+    }
+
+    public interface TimeUpdateListener{
+        void ApplyNewTime(int newTime);
+    }
+
+
+
+
+
+    //no idea, pulled from stack overflow
+    //https://stackoverflow.com/questions/14212518/is-there-a-way-to-define-a-min-and-max-value-for-edittext-in-android
+    public class InputFilterMinMax implements InputFilter {
+
+        private int min, max;
+
+        public InputFilterMinMax(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+
+        public InputFilterMinMax(String min, String max) {
+            this.min = Integer.parseInt(min);
+            this.max = Integer.parseInt(max);
+        }
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+            try {
+                int input = Integer.parseInt(dest.toString() + source.toString());
+                if (isInRange(min, max, input))
+                    return null;
+            } catch (NumberFormatException nfe) { }
+            return "";
+        }
+
+        private boolean isInRange(int a, int b, int c) {
+            return b > a ? c >= a && c <= b : c >= b && c <= a;
+        }
+    }
+}
+
